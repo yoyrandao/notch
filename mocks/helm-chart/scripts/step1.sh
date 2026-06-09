ae85d5a3ed0ed2be25b9e04e9fa7b47a40ba8898
Ilya Gubanov <yoyrandao@gmail.com>
2026-06-09 14:04:32 +0300

    fix(tests): add mocks for publishing

---
+++ mocks/helm-chart/scripts/step1.sh
@@ -0,0 +1,3 @@
+echo "$NOTCH_VERSION releasing"
+sleep 5
+echo "$NOTCH_VERSION released ($NOTCH_COMMIT)."
---
+++ mocks/helm-chart/scripts/step2.sh
@@ -0,0 +1,4 @@
+echo "notifying about new version $NOTCH_TAG..."
+echo "full changelog path: $NOTCH_CHANGELOG_PATH (repo: $NOTCH_REPOSITORY)"
+sleep 5
+echo "notified."
